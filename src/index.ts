eb4f4116b7457e41a1ce9eaf8d8442e51cf67a6e
Daris02 <hei.raymond.2@gmail.com>
2025-06-11 08:20:30 +0300

    feat: wrap to function

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,32 @@ import { Game } from "./game";
 import { Direction } from "./direction";
 import * as readline from "readline";
 
-const game = new Game();
-game.startGame();
+function launchGame() {
+  const game = new Game();
+  game.startGame();
 
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
+  readline.emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
 
-process.stdin.on("keypress", (str, key) => {
-  // if (key.ctrl && key.name === "c") {
-  //   process.exit();
-  // }
-  switch (key.name) {
-    case "w":
-      game.changeDirection(Direction.UP);
-      break;
-    case "a":
-      game.changeDirection(Direction.LEFT);
-      break;
-    case "s":
-      game.changeDirection(Direction.DOWN);
-      break;
-    case "d":
-      game.changeDirection(Direction.RIGHT);
-      break;
-  }
-});
+  process.stdin.on("keypress", (str, key) => {
+    if (key.ctrl && key.name === "c") {
+      process.exit();
+    }
+    switch (key.name) {
+      case "w":
+        game.changeDirection(Direction.UP);
+        break;
+      case "a":
+        game.changeDirection(Direction.LEFT);
+        break;
+      case "s":
+        game.changeDirection(Direction.DOWN);
+        break;
+      case "d":
+        game.changeDirection(Direction.RIGHT);
+        break;
+    }
+  });
+}
+
+launchGame();
